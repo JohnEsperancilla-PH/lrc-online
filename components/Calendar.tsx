ffc5cd44871495284da36b63e2be0c50a4845802
JohnEsperancilla-PH <business.jlte@gmail.com>
2025-09-03 14:41:13 +0800

    home page completed

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -15,7 +15,7 @@ export default function Calendar() {
               <p>
                 Check library schedules, including operating hours and events, and conveniently reserve facilities, equipment, or study spaces. Designed to help you plan ahead and make the most of the library's resources for your academic and research needs.
               </p>
-              <a className="btn small" href="#" aria-label="Reserve here">RESERVE HERE</a>
+              <a className="btn small" href="https://docs.google.com/forms/d/e/1FAIpQLSckaJPQkGKNptZJ584FywgXcvY-JLIUTNeeLU0kehmM-KLmLw/viewform?usp=sf_link" aria-label="Reserve here">RESERVE HERE</a>
             </div>
             <div
               style={{
